d5b1525311f27c6bb4a4b407388ad6712dff24eb
bob <bob910078@gmail.com>
2020-11-14 21:31:50 +0800

    完成將資料顯示在操作介面上。

--- OpenWeather/ViewController.swift
+++ OpenWeather/ViewController.swift
@@ -9,15 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let myCity = "taipei"
+        
         let service = WeatherService()
-        service.getCurrentWeatherData()
+        service.getCurrentWeatherData(cityName: myCity) { dataModel in
+            DispatchQueue.main.async {
+                // update UI here ~
+                if let model = dataModel, let weather = model.weather.first {
+                    self.myTextLabel.text = weather.main
+                    let imgURLstring = "https://openweathermap.org/img/wn/\(weather.icon).png"
+                    self.updateImage(with: imgURLstring)
+                } else {
+                    self.myTextLabel.text = "No weather data."
+                }
+                
+            }
+        }
         
     }
-
-
+    
+    
+    private func updateImage(with imageURL: String) {
+        let url = URL(string: imageURL)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let data = data, let image = UIImage(data: data) {
+                    self.myImageView.image = image
+                } else {
+                    self.myImageView.backgroundColor = .red
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    
 }
 
--- OpenWeather/WeatherService.swift
+++ OpenWeather/WeatherService.swift
@@ -9,18 +9,25 @@ import Foundation
 
 class WeatherService {
     
-    func getCurrentWeatherData() {
+    func getCurrentWeatherData(cityName: String, completion: @escaping (CurrentWeatherModel?) -> Void) {
         
-        let cityName = "taipei"
         let api_key = "05a1f2ada9a00ef2a30138d26e5814e4"
         let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(api_key)"
         let url = URL(string: urlString)!
         
         let task = URLSession.shared.dataTask(with: url) { (dataOrNil, responseOrNil, errorOrNil) in
             if let data = dataOrNil {
-                print(String(data: data, encoding: .utf8))
+                
+                do {
+                    let dataModel = try JSONDecoder().decode(CurrentWeatherModel.self, from: data)
+                    completion(dataModel)
+                } catch {
+                    completion(nil)
+                }
+                
             } else {
                 print(errorOrNil!)
+                completion(nil)
             }
         }
         
